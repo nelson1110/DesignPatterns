3fd3dd75645eae9b9dd5add7957e9a41e999de71
nelson <nelson2014@qq.com>
2017-05-02 10:21:18 +0800

    创建项目

---
+++ src/sixprinciple/Principles.java
@@ -0,0 +1,18 @@
+package sixprinciple;
+
+/**
+ * Created by Niu on 2017/5/2.
+ */
+public class Principles {
+
+/**    一：单一职责原则{@link SingleResponsibility}
+*      二：开闭原则
+*      三：里氏替换原则
+*      四：依赖倒置原则
+*      五：接口隔离原则
+*      六：迪米特原则
+*/
+
+
+
+}
---
+++ src/sixprinciple/SingleResponsibility.java
@@ -0,0 +1,10 @@
+package sixprinciple;
+
+/**
+ * Created by Niu on 2017/5/2.
+ * 单一职责
+ */
+public class SingleResponsibility {
+
+
+}
